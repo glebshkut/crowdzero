80d520ed41ad6815af15209303ce0e247f6bbe01
unnati-wq <uud@seas.upenn.edu>
2024-02-17 23:47:47 +0530

    config edits

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,6 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
+import { NetworksUserConfig } from "hardhat/types/config";
 import { ethers } from "ethers";
+
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
@@ -11,8 +13,34 @@ const config: HardhatUserConfig = {
         version: "0.8.20",
       },
     ],
-  }
-
+  },
+  networks: getNetworks(),
+  etherscan: {
+    apiKey: {
+      "scroll-sepolia": "3JSKI3SBEJG9RZVZUNAS6XC471MV2BFZ8R",
+    },
+    customChains: [
+      {
+        network: "scroll-sepolia",
+        chainId: 534351,
+        urls: {
+          apiURL: "https://api-sepolia.scrollscan.com/api",
+          browserURL: "https://sepolia.scrollscan.com/",
+        },
+      },
+    ],
+  },
 };
+
+function getNetworks(): NetworksUserConfig {
+  const accounts = [`0x${process.env.ETHEREUM_PRIVATE_KEY}`];
+  return {
+    "scroll-sepolia": {
+      url: "https://sepolia-rpc.scroll.io/",
+      accounts,
+    },
+    // Add other networks if needed
+  };
+}
 
 export default config;
