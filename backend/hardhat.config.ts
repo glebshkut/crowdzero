9bdb568ef0187abf126c5db506a76a7714866aa3
unnati-wq <uud@seas.upenn.edu>
2024-02-17 18:22:39 +0530

    config edits

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,6 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+import { ethers } from "ethers";
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
